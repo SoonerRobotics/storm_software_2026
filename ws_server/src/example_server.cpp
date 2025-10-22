804753af4d827557dbcab0535853c6378764ec46
Tony Chappell <antoniochappell@protonmail.com>
2025-10-21 20:07:19 -0500

    created my_server which uses coroutines and an event loop to do other work while handling a ws connection. Can be extended to async pub sub.

--- ws_server/src/example_server.cpp
+++ ws_server/src/example_server.cpp
@@ -24,6 +24,8 @@
 #include <thread>
 #include <vector>
 #include <future>
+#include <vector>
+#include <future>
 
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
