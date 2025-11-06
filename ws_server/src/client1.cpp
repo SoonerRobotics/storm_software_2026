cc946d6e2ea5b68c4bee2da7a8b3eaffe6a67af9
Tony Chappell <antoniochappell@protonmail.com>
2025-11-05 19:00:34 -0600

    finished the relay server and a 2 client example

--- ws_server/src/client1.cpp
+++ ws_server/src/client1.cpp
@@ -3,9 +3,12 @@
 using namespace hv;
 
 struct addressedData {
-    std::string address;
+    std::string sender;
+    std::string destination;
     std::string data;
 };
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(addressedData, sender, destination, data);
 
 int main(int argc, char **argv)
 {
@@ -14,17 +17,26 @@ int main(int argc, char **argv)
   {
     printf("onopen\n");
     addressedData ad;
-    ad.address = ws.channel.get()->localaddr();
-    ad.data = "Hello, server!";
+    ad.sender = "client1";
+    ad.destination = "init";
+    ad.data = "init";
 
-    Json j;
-    j["address"] = ad.address;
-    j["data"] = ad.data;
+    Json j = ad;
     ws.send(j.dump());
   };
-  ws.onmessage = [](const std::string &msg)
+  ws.onmessage = [&](const std::string &msg)
   {
-    printf("onmessage: %.*s\n", (int)msg.size(), msg.data());
+    addressedData received_ad = Json::parse(msg);
+    std::cout << "heard: " << received_ad.data << std::endl;
+
+    // send a message to client2
+    addressedData ad;
+    ad.sender = "client1";
+    ad.destination = "client2";
+    ad.data = "Hello, client2!";
+
+    Json j = ad;
+    ws.send(j.dump());
   };
   ws.onclose = []()
   {
@@ -39,7 +51,7 @@ int main(int argc, char **argv)
   reconn.delay_policy = 2;
   ws.setReconnect(&reconn);
 
-  ws.open("ws://127.0.0.1:9999");
+  ws.open("ws://127.0.0.1:1909");
 
   std::string str;
   while (std::getline(std::cin, str))
--- ws_server/src/relay_server.cpp
+++ ws_server/src/relay_server.cpp
@@ -1,153 +1,72 @@
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/json.hpp>
-#include <iostream>
-#include <future>
-#include <boost/asio.hpp>
+#include "hv/WebSocketServer.h"
+#include "hv/json.hpp"
 #include <unordered_map>
 
-#define SERVER_IP "0.0.0.0"
-
-#define SERVER_PORT "8080"
-
-std::unordered_map<std::string, std::shared_ptr<boost::beast::websocket::stream<boost::asio::ip::tcp::socket>>> websocket_table;
+using namespace hv;
 
 struct addressedData {
-  std::string client_id;
-  std::string jsonMsgData;
+    std::string sender;
+    std::string destination;
+    std::string data;
 };
 
-void tag_invoke(boost::json::value_from_tag, boost::json::value &jv, const addressedData &s)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(addressedData, sender, destination, data);
+
+std::unordered_map<std::string, WebSocketChannelPtr> connections_map;
+
+int main(int argc, char **argv)
 {
-  jv = {
-      {"client_id", s.client_id},
-      {"jsonMsgData", s.jsonMsgData}};
-}
+  WebSocketService ws;
+  ws.onopen = [](const WebSocketChannelPtr &channel, const HttpRequestPtr &req)
+  {
+    printf("onopen: GET %s\n", req->Path().c_str());
+  };
+  ws.onmessage = [](const WebSocketChannelPtr &channel, const std::string &msg)
+  {
+    Json j = Json::parse(msg);
+    addressedData ad = j;
 
-addressedData tag_invoke(boost::json::value_to_tag<addressedData>, const boost::json::value &jv)
-{
-  const auto obj = jv.as_object();
-  return addressedData{
-      boost::json::value_to<std::string>(obj.at("client_id")),
-      boost::json::value_to<std::string>(obj.at("jsonMsgData"))};
-}
+    std::cout << "onmessage from " << ad.sender << ": " << ad.data << std::endl;
 
-boost::asio::awaitable<void> forward(const std::string &client_id, const std::string &message) {
-
-  auto ex = co_await boost::asio::this_coro::executor;
-
-  std::cout << "forwarding message to client_id: " << client_id << " message: " << message << std::endl;
-
-  auto it = websocket_table.find(client_id);
-  if (it != websocket_table.end()) {
-    auto socket_ptr = it->second;
-
-    boost::beast::multi_buffer buffer;
-
-    addressedData returnMsg {
-      client_id,
-      message
-    };
-
-    boost::json::value jv = boost::json::value_from(returnMsg);
-
-    std::string data = boost::json::serialize(jv);
-
-    std::cout << "forwarding data: " << data << std::endl;
-
-    co_await socket_ptr->async_write(boost::asio::buffer(data), boost::asio::use_awaitable);
-  } else {
-    std::cerr << "Client ID not found: " << client_id << std::endl;
-  }
-}
-
-boost::asio::awaitable<void> do_session(const std::string &client_id) {
-  auto ex = co_await boost::asio::this_coro::executor;
-
-  std::cout << "starting session" << std::endl;
-
-  // read a message from the websocket
-  auto it = websocket_table.find(client_id);
-  if (it == websocket_table.end()) {
-    std::cerr << "Client ID not found: " << client_id << std::endl;
-    co_return;
-  }
-
-  auto socket_ptr = it->second;
-
-  boost::beast::multi_buffer buffer;
-
-  try {
-    for (;;) {
-      co_await socket_ptr->async_read(buffer, boost::asio::use_awaitable);
-      std::string data = boost::beast::buffers_to_string(buffer.data());
-      std::cout << "received: " << data << std::endl;
-
-      // parse the message
-      boost::json::value jv = boost::json::parse(data);
-      addressedData received = boost::json::value_to<addressedData>(jv);
-
-      // call forward with the address and message
-      auto it = websocket_table.find(received.client_id);
-
-      boost::asio::co_spawn(ex, forward(received.client_id, received.jsonMsgData), boost::asio::detached);
-
-      buffer.consume(buffer.size());
+    // if the sender is new, save the connection
+    if (connections_map.find(ad.sender) == connections_map.end()) {
+        std::cout << "new sender: " << ad.sender << std::endl;
+        connections_map[ad.sender] = channel;
     }
-  }
-  catch (boost::beast::system_error const &se)
+    
+    WebSocketChannelPtr destination_channel;
+
+    // if the address is not known, return
+    if (connections_map.find(ad.destination) == connections_map.end()) {
+        std::cout << "unknown address: " << ad.destination << std::endl;
+        return;
+    } else {
+        destination_channel = connections_map[ad.destination];
+    }
+
+    ad.data = ad.data;
+    j = ad;
+
+    // send the message to the address specified in the message
+    destination_channel->send(j.dump());
+  };
+  ws.onclose = [&](const WebSocketChannelPtr &channel)
   {
-    // This indicates that the session was closed
-    if (se.code() != boost::beast::websocket::error::closed)
-      std::cerr << "Error: " << se.code().message() << std::endl;
-  }
-  catch (std::exception const &e)
-  {
-    std::cerr << "Error: " << e.what() << std::endl;
-  }
-}
+    for (auto it = connections_map.begin(); it != connections_map.end();)
+    {
+      if (it->second == channel)
+        std::cout << "removing connection for " << it->first << "\n",
+        it = connections_map.erase(it);
+      else
+        it++;
+    }
+    for (auto &[k, v] : connections_map)
+      std::cout << "still connected: " << k << std::endl;
+  };
 
-boost::asio::awaitable<void> event_loop(boost::asio::ip::tcp::acceptor &acceptor)
-{
-  auto ex = co_await boost::asio::this_coro::executor;
-
-  std::cout << "event loop running" << std::endl;
-
-  for (;;)
-  {
-    boost::asio::ip::tcp::socket socket{ex};
-    co_await acceptor.async_accept(socket, boost::asio::use_awaitable);
-
-    auto ws_ptr = std::make_shared<boost::beast::websocket::stream<boost::asio::ip::tcp::socket>>(std::move(socket));
-
-    co_await ws_ptr->async_accept(boost::asio::use_awaitable);
-
-    std::string client_id = ws_ptr->next_layer().remote_endpoint().address().to_string() + ":" +
-                            std::to_string(ws_ptr->next_layer().remote_endpoint().port());
-
-    websocket_table.emplace(client_id, ws_ptr);
-
-    boost::asio::co_spawn(ex, do_session(client_id), boost::asio::detached);
-
-    std::cout << "New client connected: " << client_id << std::endl;
-  }
-}
-
-int main () {
-  // a hashmap of websockets
-  // in the event loop, define an acceptor. When a new client connects, add the websocket to the hashmap
-  // in forward(), lookup the ws by its id (address) and
-
-  boost::asio::io_context ioc{1};
-
-  const auto address = boost::asio::ip::make_address(SERVER_IP);
-
-  const auto port = static_cast<unsigned short>(std::atoi(SERVER_PORT));
-
-  //acceptor
-  boost::asio::ip::tcp::acceptor acceptor{ioc, {address, port}};
-
-  std::future<void> f = boost::asio::co_spawn(ioc, event_loop(acceptor), boost::asio::use_future);
-
-  ioc.run();
+  WebSocketServer server(&ws);
+  server.setPort(1909);
+  server.setThreadNum(4);
+  server.run();
+  return 0;
 }
