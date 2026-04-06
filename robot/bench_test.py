043a14d801688d6cadf2a63f350eca0e53d7b9fb
Daniel Brown <is.a.squrril@gmail.com>
2026-04-06 01:34:00 -0500

    threw together a bench test because the pico is doing weird things

---
+++ robot/bench_test.py
@@ -0,0 +1,57 @@
+# this file is meant to run from your computer, plugged directly into the Pico 2
+# essentially acting as the robot and base station all in one
+
+import pygame
+import struct
+import serial
+
+def main():
+    # init pygame
+    pygame.init()
+
+    # open serial
+    pico = serial.Serial("/dev/ttyACM0", 115200, timeout=0.5)
+
+    # connect joystick
+    joystick = pygame.joystick.Joystick(0)
+    timer = pygame.time.Clock()
+
+    # pygame events loop FIXME
+    # for event in pygame.event.get():
+
+    # our own event loop?
+    try:
+        while True:
+            # get joystick input FIXME
+            msg = struct.pack(
+                ">c11Bc",
+                b"$",
+                # joystick.get_axis(0),
+                # joystick.get_axis(1),
+                # joystick.get_axis(2),
+                # joystick.get_axis(3),
+                127,
+                127,
+                127,
+                127,
+                127, # slide
+                127, # intake
+                127, # arm
+                127, # wrist
+                127, # claw
+                127, # climber
+                2, # jumpstart
+                b"!"
+            )
+
+            # send serial message
+            pico.write(msg)
+
+            # don't overload
+            timer.tick(50)
+    finally:    
+        # close serial
+        pico.close()
+
+if __name__ == "__main__":
+    main()
