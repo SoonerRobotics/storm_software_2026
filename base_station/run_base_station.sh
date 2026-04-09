6aed59636221ac8d0d6eb4a222faa45769022076
SCR <scr@ou.edu>
2026-04-09 17:50:50 -0500

    base station run script actually works now

--- base_station/run_base_station.sh
+++ base_station/run_base_station.sh
@@ -1,4 +1,17 @@
 echo "RUNNING BASE STATION"
 
+# change directory
+cd ~/Documents/storm_software_2026/base_station
+
+# activate virtual environment
+source ../venv/bin/activate
+
+# run websocket relay
+gnome-terminal --command ../build/relay_server &
+
+sleep 1
+
+# run python scripts
 python3 ./controller_client.py &
-python3 ./StormGUI.py
+python3 ./AprilTagPoses.py &
+python3 ./storm_logging.py
