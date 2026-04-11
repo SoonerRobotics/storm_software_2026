7fa9c308256920afbb8fef9e34c289ae7051ae4e
Daniel Brown <is.a.squrril@gmail.com>
2026-04-10 23:02:58 -0500

    climb bad

--- robot/robot.py
+++ robot/robot.py
@@ -531,11 +531,7 @@ class RobotClient:
             cmd.arm_extend_motor = constants["SLIDE_STOW_SPEED"]
 
         # Climber retract/extend
-        if s.button_y or self.last_controller_state.button_y: # only while held for safety
-            cmd.climb_motor_speed = constants["CLIMB_SPEED"]
-        elif not s.button_y and not self.last_controller_state.button_y:
-            # only stop after 2 consecutive not-presses?
-            cmd.climb_motor_speed = 0.0 #FIXME we might need to run it backwards too? maybe?
+        cmd.climb_motor_speed = s.trigger_left - s.trigger_right
 
         # Jumpstart voltage FIXME this just runs 100% of the time lol
         cmd.jumpstart_voltage = self.voltage_setpoint
